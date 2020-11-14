8fdb36edd7bed6779fe2cb262767d114a4244c38
PiotrSienkiewicz <piotr@loop13.com>
2020-11-14 21:19:45 +0100

    chged method of showing initial value of gear

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ bikeState.on('simpower', (simpower) => {
 // first state
 bikeState.setGear(4);
 //io.emit('gear', gear); PS: this my addition causes an error - because gear is not defined!
+socket.emit('key', 'GearDn');
 
 //--- Serial port
 var kettlerUSB = new kettlerUSB();
